b07e57ca46bf98bdfee5e9511d9bcb6bf85158c0
Tsuyoshi HARA <chloe463+git@gmail.com>
2021-04-28 02:58:18 +0900

    change(post): Use prisma to fetch posts

--- src/context.ts
+++ src/context.ts
@@ -1,12 +1,15 @@
+import { PrismaClient } from "@prisma/client";
 import { ApolloServerExpressConfig } from "apollo-server-express";
 import { JsonPlaceholderClient } from "./api/JsonPlaceholderClient";
 
 export type Context = {
   jsonPlaceholderClient: JsonPlaceholderClient,
+  prismaClient: PrismaClient,
 };
 
 export const createContext: ApolloServerExpressConfig["context"] = () => {
   return {
     jsonPlaceholderClient: new JsonPlaceholderClient(),
+    prismaClient: new PrismaClient(),
   };
 };
