f629a69379a7c73544b2b478f9b4755a3d4f0333
Tsuyoshi HARA <chloe463+git@gmail.com>
2021-04-30 01:26:46 +0900

    feat(seed): Add seeds to create questionnaire records

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -2,7 +2,7 @@ import { PrismaClient } from "@prisma/client";
 import faker from "faker";
 const prisma = new PrismaClient();
 
-async function main() {
+const createPosts = async () =>{
   const posts = Array.from({ length: 100 }, (_, i) => i + 1).map((i: number) => {
     return {
       title: faker.lorem.word(10),
@@ -15,6 +15,55 @@ async function main() {
       data: post,
     });
   }
+};
+
+const createQuestionnares = async () => {
+  const options = Array.from({ length: 4 }, (_, i) => i + 1).map((i: number) => {
+    return {
+      text: faker.lorem.word(8),
+      createdAt: new Date(),
+      updatedAt: new Date(),
+    };
+  });
+
+  const questions = Array.from({ length: 4 }, (_, i) => i + 1).map((i: number) => {
+    return {
+      text: faker.lorem.word(8),
+      type: i % 4,
+      createdAt: new Date(),
+      updatedAt: new Date(),
+      options: {
+        create: options,
+      },
+    };
+  });
+
+  const questionnaires = Array.from({ length: 10 }, (_, i) => i + 1).map((i: number) => {
+    const startAt = new Date();
+    const endAt = new Date(startAt.getFullYear(), startAt.getMonth() + 2, startAt.getDate());
+    return {
+      title: `Questionnaire.${i}`,
+      description: faker.lorem.words(30),
+      state: i % 4,
+      startAt,
+      endAt,
+      createdAt: new Date(),
+      updatedAt: new Date(),
+      questions: {
+        create: questions,
+      },
+    };
+  });
+  for (const questionnaire of questionnaires) {
+    await prisma.questionnaire.create({
+      data: questionnaire,
+    });
+  }
+};
+
+async function main() {
+  await createPosts();
+  await createQuestionnares();
 };
 
 main()
