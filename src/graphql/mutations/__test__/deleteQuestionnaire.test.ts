afd69c5f60b8ba16a303a2df5a89fb0f4bc9d082
Tsuyoshi HARA <chloe463+git@gmail.com>
2021-05-04 02:23:24 +0900

    test(deleteQuestionnaire): Add test

--- src/graphql/mutations/__test__/deleteQuestionnaire.test.ts
+++ src/graphql/mutations/__test__/deleteQuestionnaire.test.ts
@@ -1,13 +1,41 @@
 import { PrismaClient } from "@prisma/client";
-import { NexusGenInputs, NexusGenObjects } from "../../generated/typings";
+import { gql } from "apollo-server";
+import { createTestClient } from "apollo-server-testing";
+import { constructTestServer } from "../../../testUtils";
 
 const prismaClientMock = (PrismaClient as any) as jest.Mock<PrismaClient>;
 
-type Variables = NexusGenInputs["CreateQuestionnaireInput"];
-type Result = NexusGenObjects["CreateQuestionnairePayload"]["questionnaire"];
+type DeleteQuestionnaireVariables = {
+  id: number;
+}
+type DeleteQuestionnaireResponse = {
+  result: boolean;
+};
 
+const DELETE_QUESTIONNAIRE_MUTATION = gql`
+  mutation DeleteQuestionnaire($id: Int!) {
+    deleteQuestionnaire(id: $id) {
+      result
+    }
+  }
+`;
 
 describe("[Mutation] deleteQuestionnaire", () => {
-  it.skip("can soft delete questionnaire", async () => {
+  it("can soft delete questionnaire", async () => {
+    (prismaClientMock as any).questionnaire = {
+      update: jest.fn().mockReturnValue(true),
+    };
+    const server = constructTestServer({
+      prismaClient: prismaClientMock,
+    });
+
+    const { mutate } = createTestClient(server);
+    const res = await mutate<DeleteQuestionnaireResponse, DeleteQuestionnaireVariables>({
+      mutation: DELETE_QUESTIONNAIRE_MUTATION,
+      variables: {
+        id: 1,
+      },
+    });
+    expect(res).toMatchSnapshot();
   });
 });
