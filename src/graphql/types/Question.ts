63e10997e4a96001791b989fa86a792f2c4d6ccf
Tsuyoshi HARA <chloe463+git@gmail.com>
2023-06-05 22:16:21 +0900

    change(Question): Make question type enum

--- src/graphql/types/Question.ts
+++ src/graphql/types/Question.ts
@@ -1,18 +1,38 @@
-import { objectType } from "nexus";
+import { enumType, objectType } from "nexus";
 import { Option } from "./Option";
+
+type NexusEnumTypeConfig = Parameters<typeof enumType>[0];
+
+const members: NexusEnumTypeConfig["members"] = [
+  { name: "CHECKBOX" },
+  { name: "RADIO" },
+  { name: "SELECT" },
+  { name: "TEXT" },
+];
+
+export const QuestionType = enumType({
+  name: "QuestionType",
+  members,
+});
 
 export const Question = objectType({
   name: "Question",
   definition(t) {
     t.nonNull.int("id");
-    t.nonNull.int("type");
+    t.nonNull.field("type", {
+      type: QuestionType,
+      resolve: (question) => {
+        const questionTypeNum = (question as unknown as any).type
+        return (members[questionTypeNum] as unknown as any).name;
+      },
+    });
     t.nonNull.string("text");
     t.nonNull.list.field("options", {
       type: Option,
       resolve: async (root, args, context) => {
         const { optionsLoader } = context;
         return await optionsLoader.load(root.id);
-      }
+      },
     });
   },
 });
