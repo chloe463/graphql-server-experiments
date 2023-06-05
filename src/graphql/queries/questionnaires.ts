3113d123341b300a2a7ae4e8c79fc7fd255e5bac
Tsuyoshi HARA <chloe463+git@gmail.com>
2023-06-05 22:16:43 +0900

    change(questionnaires): Add page arg

--- src/graphql/queries/questionnaires.ts
+++ src/graphql/queries/questionnaires.ts
@@ -1,9 +1,13 @@
-import { list, queryField } from "nexus";
+import { intArg, list, queryField } from "nexus";
 
 export const questionnaires = queryField("questionnaires", {
   type: list("Questionnaire"),
+  args: {
+    page: intArg({ default: 1 }),
+  },
   resolve: async(_root, args, context) => {
     const { prismaClient } = context;
+    const { page } = args;
     const questionnaires = await prismaClient.questionnaire.findMany({
       where: {
         deleted: {
@@ -11,6 +15,8 @@ export const questionnaires = queryField("questionnaires", {
         },
       },
       include: { questions: true,  },
+      skip: (page - 1) * 10,
+      take: 10,
     });
     return questionnaires;
   },
